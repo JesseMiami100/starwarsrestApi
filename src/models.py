0bdf347ac23463f653bcb9460c1e5b3e7847ef37
JesseMiami100 <jessemetayer@gmail.com>
2023-05-24 00:05:08 +0000

    firstcommit

--- src/models.py
+++ src/models.py
@@ -17,3 +17,45 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    eye_color = db.Column(db.String(80), unique=False, nullable=False)
+    is_human = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.eye_color
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye_color, 
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    terrain = db.Column(db.String(80), unique=False, nullable=False)
+    gravity = db.Column(db.String(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain, 
+            # do not serialize the password, its a security breach
+        }
+
+class FavoritePeople(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self):
+        return '<FavoritePeople %r>' % self.eye_color
+
+        
